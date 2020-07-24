c00ae3be148c9aa556f5a29e315399b0fb2ded60
kaneh <kanehhh777@gmail.com>
2020-07-24 13:28:15 +0300

    aaa

--- Assets/Scripts/CameraMotion.cs
+++ Assets/Scripts/CameraMotion.cs
@@ -30,9 +30,9 @@ public class CameraMotion : MonoBehaviour
         rotationAngle += mouse_x * angularSpeed*Time.deltaTime;
         transform.Rotate(0, rotationAngle, 0);
 
-        // transform.Translate(Vector3.forward * Time.deltaTime * speed);;
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed);
         Vector3 direction=transform.TransformDirection(Vector3.forward*Time.deltaTime*speed);
         characterController.Move(direction);
-
+       
     }
 }
